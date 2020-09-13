9bb3771380d56d96465c345717a606e636dadb97
gerry <gerry.scott99@gmail.com>
2020-09-13 12:36:45 -0400

    Start of project

---
+++ cmake-build-debug/CMakeFiles/Tic_Tac_Toe_Extended.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tic_Tac_Toe_Extended.dir/main.cpp.obj"
+  "Tic_Tac_Toe_Extended.exe"
+  "Tic_Tac_Toe_Extended.exe.manifest"
+  "Tic_Tac_Toe_Extended.pdb"
+  "libTic_Tac_Toe_Extended.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tic_Tac_Toe_Extended.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
